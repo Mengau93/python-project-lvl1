24fffa650e9991fb03d0047961742ea493028ecb
Ivan Cheklin <ivan.cheklin.1985@gmail.com>
2021-06-03 13:40:15 +0300

    Modified files according to linter

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,8 +1,13 @@
 #! /usr/bin/env python
+"""Execute the program."""
+
+from brain_games.cli import welcome_user
+
 
 def main():
-    print('Welcome to the Brain Games!')
+    """Execute the function from cli.py."""
+    welcome_user()
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     main()
-
