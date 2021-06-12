f2a7feabe0b7278237813d6b2c0ee8135de172c5
Ivan Cheklin <ivan.cheklin.1985@gmail.com>
2021-06-12 17:18:51 +0300

    Refactor the brain-calc game

--- brain_games/scripts/games/brain_calc.py
+++ brain_games/scripts/games/brain_calc.py
@@ -1,7 +1,15 @@
 """Start simple calculator game."""
 import prompt
-from brain_games.scripts.user_welcome import welcome_user
+from brain_games.scripts.user_prompts import welcome_user
+from brain_games.scripts.user_prompts import print_wrong
+from brain_games.scripts.user_prompts import print_correct
 from brain_games.scripts.calculations import randomization, two_args_calc
+
+
+def question_generate():
+    data = randomization()
+    correct_ans = two_args_calc(data)
+    return data, str(correct_ans)
 
 
 def main():
@@ -11,22 +19,17 @@ def main():
     summary_counter = 0
     correct_counter = 0
     while summary_counter < 3:
-        data = randomization()
-        correct_ans = two_args_calc(data)
+        data, correct_ans = question_generate()
         print('Question: {0}{1}{2}'.format(*data))
         ans = prompt.string('Your answer: ')
-        if not ans[1:].isdigit() and ans[0] not in '+-*1234567890':
-            print('Incorrect answer!')
-        elif int(ans) == correct_ans:
-            print('Correct!')
-            correct_counter += 1
+        if ans == correct_ans:
+            correct_counter = print_correct(correct_counter)
         else:
-            print('"{0}" is wrong answer ;(.'.format(ans), end=' ')
-            print('Correct answer was "{0}"'.format(correct_ans))
+            print_wrong(name, ans, correct_ans)
             break
         summary_counter += 1
     if correct_counter == 3:
-        print('Congratulations, {0}'.format(name))
+        print('Congratulations, {0}!'.format(name))
 
 
 if __name__ == '__main__':
