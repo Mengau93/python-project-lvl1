62e1d4405456d33e9500a2189fb42d6abac60099
Ivan Cheklin <ivan.cheklin.1985@gmail.com>
2021-06-12 17:44:58 +0300

    Refactor brain-progression game

--- brain_games/scripts/games/brain_progression.py
+++ brain_games/scripts/games/brain_progression.py
@@ -1,8 +1,18 @@
 """Start simple progression game."""
 import prompt
 import random
-from brain_games.scripts.user_welcome import welcome_user
+from brain_games.scripts.user_prompts import welcome_user
+from brain_games.scripts.user_prompts import print_wrong
+from brain_games.scripts.user_prompts import print_correct
 from brain_games.scripts.calculations import arythm_progression_generate as apg
+
+
+def question_generate():
+    data = apg()
+    hidden_index = random.randint(0, len(data) - 1)
+    correct_ans = data[hidden_index]
+    data[hidden_index] = ".."
+    return data, str(correct_ans)
 
 
 def main():
@@ -12,24 +22,17 @@ def main():
     summary_counter = 0
     correct_counter = 0
     while summary_counter < 3:
-        data = apg()
-        hidden_index = random.randint(0, len(data) - 1)
-        correct_ans = data[hidden_index]
-        data[hidden_index] = ".."
+        data, correct_ans = question_generate()
         print('Question:', *data)
         ans = prompt.string('Your answer: ')
-        if not ans.isdigit():
-            print("Incorrect answer")
-        elif int(ans) == correct_ans:
-            print('Correct!')
-            correct_counter += 1
+        if ans == correct_ans:
+            correct_counter = print_correct(correct_counter)
         else:
-            print('"{0}" is wrong answer ;(.'.format(ans), end=' ')
-            print('Correct answer was "{0}"'.format(correct_ans))
+            print_wrong(name, ans, correct_ans)
             break
         summary_counter += 1
     if correct_counter == 3:
-        print('Congratulations, {0}'.format(name))
+        print('Congratulations, {0}!'.format(name))
 
 
 if __name__ == '__main__':
