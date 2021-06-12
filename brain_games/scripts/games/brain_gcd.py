95f745677729c2a4ae5cc4c57b8b6006fdcc0736
Ivan Cheklin <ivan.cheklin.1985@gmail.com>
2021-06-12 17:35:05 +0300

    Refactor brain-gcd game

--- brain_games/scripts/games/brain_gcd.py
+++ brain_games/scripts/games/brain_gcd.py
@@ -1,7 +1,15 @@
-"""Start simple gcd  game."""
+"""Start simple gcd game."""
 import prompt
-from brain_games.scripts.user_welcome import welcome_user
+from brain_games.scripts.user_prompts import welcome_user
+from brain_games.scripts.user_prompts import print_wrong
+from brain_games.scripts.user_prompts import print_correct
 from brain_games.scripts.calculations import gcd, two_random_nums
+
+
+def question_generate():
+    data = two_random_nums()
+    correct_ans = gcd(*data)
+    return data, str(correct_ans)
 
 
 def main():
@@ -11,22 +19,17 @@ def main():
     summary_counter = 0
     correct_counter = 0
     while summary_counter < 3:
-        data = two_random_nums()
-        correct_ans = gcd(*data)
+        data, correct_ans = question_generate()
         print('Question: {0} {1}'.format(*data))
         ans = prompt.string('Your answer: ')
-        if not ans.isdigit():
-            print("Incorrect answer")
-        elif int(ans) == correct_ans:
-            print('Correct!')
-            correct_counter += 1
+        if ans == correct_ans:
+            correct_counter = print_correct(correct_counter)
         else:
-            print('"{0}" is wrong answer ;(.'.format(ans), end=' ')
-            print('Correct answer was "{0}"'.format(correct_ans))
+            print_wrong(name, ans, correct_ans)
             break
         summary_counter += 1
     if correct_counter == 3:
-        print('Congratulations, {0}'.format(name))
+        print('Congratulations, {0}!'.format(name))
 
 
 if __name__ == '__main__':
