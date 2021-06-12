e1585ce060c262445b0491e0e2b445f87e64834c
Ivan Cheklin <ivan.cheklin.1985@gmail.com>
2021-06-12 17:03:48 +0300

    Refactor brain-even game

--- brain_games/scripts/games/brain_even.py
+++ brain_games/scripts/games/brain_even.py
@@ -1,32 +1,34 @@
 """Start simple even-odd game."""
 import prompt
 import random
-from brain_games.scripts.user_welcome import welcome_user
+from brain_games.scripts.user_prompts import welcome_user
+from brain_games.scripts.user_prompts import print_wrong
+from brain_games.scripts.user_prompts import print_correct
+
+
+def question_generate():
+    x = random.randint(0, 100)
+    correct_ans = 'yes' if x % 2 == 0 else 'no'
+    return x, correct_ans
 
 
 def main():
     """Start the game."""
     name = welcome_user()
-    print('Answer "yes" if the number is even, otherwise answer "no"')
+    print('Answer "yes" if the number is even, otherwise answer "no".')
     summary_counter = 0
-    correct_answers = 0
+    correct_counter = 0
     while summary_counter < 3:
-        x = random.randint(0, 100)
+        x, correct_ans = question_generate()
         print('Question: {0}'.format(x))
         ans = prompt.string('Your answer: ')
-        if x % 2 == 0 and ans == 'yes' or x % 2 == 1 and ans == 'no':
-            print('Correct!')
-            correct_answers += 1
+        if ans == correct_ans:
+            correct_counter = print_correct(correct_counter)
         else:
-            if ans == 'yes':
-                print("'yes' is wrong answer ;(. Correct answer was 'no'")
-            elif ans == 'no':
-                print("'no' is wrong answer ;(. Correct answer was 'yes'")
-            else:
-                print('Your answer is wrong ;(.')
+            print_wrong(name, ans, correct_ans)
             break
         summary_counter += 1
-    if correct_answers == 3:
+    if correct_counter == 3:
         print('Congratulations, {0}!'.format(name))
 
 
